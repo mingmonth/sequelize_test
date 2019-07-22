a73b91d3fc3c4c2442dd60f8bc282b156f724e26
mingmonth <ys9922@naver.com>
2019-07-23 00:10:25 +0900

    1번 방법

---
+++ routes/userManager.js
@@ -0,0 +1,16 @@
+var express = require("express");
+
+var User = require("../models").User;
+var router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    const users = await User.findAll();
+    res.render("userManager", { users });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+module.exports = router;
